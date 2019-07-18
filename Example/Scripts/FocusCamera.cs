e0f9368b62d9286c33355664ac2222d389db97bf
Tacohej <mctinn@gmail.com>
2019-07-18 07:14:33 +0200

    example scripts

---
+++ Example/Scripts/FocusCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FocusCamera : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject focusObject;
+
+    private Vector3 offsetVector;
+
+    void Start()
+    {
+        offsetVector = this.transform.position - focusObject.transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        var faceDir = focusObject.GetComponent<Player>().faceDirection;
+        var mag = focusObject.GetComponent<Player>().mag;
+        var movedir = focusObject.GetComponent<Player>().moveDirection;
+        this.transform.position = Vector3.Lerp(this.transform.position + faceDir * 0.3f * Mathf.Clamp(mag * 0.2f, 0, 3) + movedir * 0.5f, offsetVector + focusObject.transform.position, 0.2f);
+    }
+}
--- Example/Scripts/Player.cs
+++ Example/Scripts/Player.cs
@@ -8,45 +8,84 @@ public class Player : MonoBehaviour
     public int baseHealth = 100;
     public int baseStrength = 5;
     public int baseAgility = 7;
-
     public SparkUnit unit;
-    public Rigidbody rigidBody;
+    public LayerMask layerMask;
+
+    public Vector3 moveDirection = Vector3.zero;
+    public Vector3 faceDirection = Vector3.zero;
+    public float mag = 0;
+
+    public Rigidbody bulletPrefab;
+
+    public GameObject leftHand;
+    public GameObject rightHand;
+
+    private Rigidbody rigidBody;
+    private float speed = 7;
+    private Vector3 originalLeftPosition;
+    private Vector3 originalRightPosition;
+    private bool handSwitcher = true;
+    
 
     void Start ()
     {
         rigidBody = GetComponent<Rigidbody>();
-
         unit.SetBaseStat<Strength>(baseStrength);
         unit.SetBaseStat<Agility>(baseAgility);
         unit.SetBaseStat<Health>(baseHealth);
+
+        originalLeftPosition = leftHand.transform.localPosition;
+        originalRightPosition = rightHand.transform.localPosition;
     }
 
     void Update ()
     {
+        var horizontalAxis = Input.GetAxis("Horizontal");
+        var verticalAxis = Input.GetAxis("Vertical");
+        moveDirection = Vector3.ClampMagnitude(new Vector3(horizontalAxis, 0, verticalAxis), 1);
+        
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-
-        if (Input.GetMouseButton(1))
+        if (Physics.Raycast(ray, out hit, float.MaxValue, layerMask))
         {
-            // todo: ignore player on raycast
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            var offsettedPosition = new Vector3(hit.point.x, this.transform.position.y, hit.point.z);
+            mag = (offsettedPosition - this.transform.position).magnitude;
+            faceDirection = (offsettedPosition - this.transform.position).normalized;
 
-            if (Physics.Raycast(ray, out hit))
+            if (faceDirection != Vector3.zero)
             {
-                var offsettedPosition = new Vector3(hit.point.x, this.transform.position.y, hit.point.z);
-                var mag = (offsettedPosition - this.transform.position).magnitude;
-                var clampedMag = Vector3.ClampMagnitude((offsettedPosition - this.transform.position), 1).magnitude;
-                var direction = (offsettedPosition - this.transform.position).normalized;
-                Debug.Log(clampedMag);
-                var vel = Time.deltaTime * direction * 2000 * clampedMag;
-                Debug.Log(vel.magnitude);
-                rigidBody.velocity = Vector3.ClampMagnitude(vel, 20);
+                transform.rotation = Quaternion.LookRotation(faceDirection, this.transform.up);
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 offset;
+
+            if (leftHand.transform.localPosition == originalLeftPosition)
+            {
+                offset = this.transform.right * 0.5f;
+                leftHand.transform.localPosition = originalLeftPosition - new Vector3(0, 0, 0.3f);
+                rightHand.transform.localPosition = originalRightPosition;
+            } else
+            {
+                offset = -this.transform.right * 0.5f;
+                leftHand.transform.localPosition = originalLeftPosition;
+                rightHand.transform.localPosition = originalRightPosition - new Vector3(0, 0, 0.3f);
             }
 
-            this.transform.position = new Vector3(this.transform.position.x, Mathf.Abs(Mathf.Sin(Time.time * 10)), this.transform.position.z);
-        } else {
-            rigidBody.velocity = Vector3.zero;
-            this.transform.position = new Vector3(this.transform.position.x, 0, this.transform.position.z);
+            // recoil
+            rigidBody.AddForce(faceDirection * - 300);
+
+            // bullet
+            var instance = Instantiate(bulletPrefab, this.transform.position + this.transform.forward * 2 + offset, this.transform.rotation);
+            instance.AddForce(faceDirection * 1200);
         }
+    }
+
+    void FixedUpdate() 
+    {
+        rigidBody.MovePosition(rigidBody.position + moveDirection * speed * Time.fixedDeltaTime);
     }
 }
